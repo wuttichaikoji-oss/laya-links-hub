1ce6f36de351bea96c09ef589e6c52805362340c
wuttichaikoji-oss <wuttichaikoji@gmail.com>
2026-03-12 15:17:03 +0700

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,9 +0,0 @@
-window.LAYA_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB7t2WW7zmZnXVWXujKE0ayYjB-dB3MlpI",
-  authDomain: "laya-order.firebaseapp.com",
-  projectId: "laya-order",
-  storageBucket: "laya-order.firebasestorage.app",
-  messagingSenderId: "969828212139",
-  appId: "1:969828212139:web:d09782b21570f1cfa106ee",
-  measurementId: "G-KXEFLQF3HN"
-};
